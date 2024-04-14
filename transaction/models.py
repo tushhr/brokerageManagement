d7646b9037cb9828a59441caaadee957f68b827e
Tushhr <tusharmaheshwaritm6@gmail.com>
2024-04-14 16:33:00 +0530

    feat: add the transaction model

--- transaction/models.py
+++ transaction/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from organization.models import Organization
+
 # Create your models here.
+
+class Transaction(models.Model):
+     seller = models.ForeignKey('Organization', on_delete=models.CASCADE)
+     buyer = models.ForeignKey('Organization', on_delete=models.CASCADE)
+     broker = models.ForeignKey('Organization', on_delete=models.CASCADE)
+     date = models.DateField()
+     quantity = models.IntegerField()
+     weight = models.IntegerField()
+     rate = models.IntegerField()
+     brokerage_rate = models.IntegerField()
