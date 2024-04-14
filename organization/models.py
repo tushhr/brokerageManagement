ac4ce25986cff613c51c666330ae39d3bc457757
Tushhr <tusharmaheshwaritm6@gmail.com>
2024-04-14 16:19:19 +0530

    feat: add models for organization

--- organization/models.py
+++ organization/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Organization(models.Model):
+    ORG_TYPE = (
+        ('0', 'Broker'),
+        ('1', 'Client'),
+    )
+
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    mobile = models.IntegerField()
+    email = models.EmailField()
+    org_type = models.CharField(max_length=1, choices=ORG_TYPE, default='0')
+    bank = models.CharField(max_length=100)
+    bank_account = models.IntegerField()
+    rtgs_code = models.CharField(max_length=100)
+    gst_number = models.CharField(max_length=100)
