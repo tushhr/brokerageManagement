c3b8fe44c5b523461cfbc3b753aed3a8d95ae9e7
Tushhr <tusharmaheshwaritm6@gmail.com>
2024-04-14 19:49:22 +0530

    feat: add form to add transaction

--- transaction/views.py
+++ transaction/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from organization.models import Organization
+
 # Create your views here.
+def index(request):
+    clients = Organization.objects.filter(org_type='1')
+    brokers = Organization.objects.filter(org_type='0')
+    context = {
+        'clients': clients,
+        'brokers': brokers
+    }
+    return render(request, 'transaction/index.html', context=context)
