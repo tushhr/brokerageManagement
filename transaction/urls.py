9f982da014a297c60be59a9f58281ea341cd42fe
Tushhr <tusharmaheshwaritm6@gmail.com>
2024-04-14 20:05:32 +0530

    feat: add view for transaction

--- transaction/urls.py
+++ transaction/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from . import views
 
 app_name = 'transaction'
+
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('transaction', views.transaction, name='transaction'),
 ]
--- transaction/views.py
+++ transaction/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from organization.models import Organization
+from transaction.models import Transaction
 
 # Create your views here.
 def index(request):
-    clients = Organization.objects.filter(org_type='1')
-    brokers = Organization.objects.filter(org_type='0')
+    clients = Organization.objects.filter(org_type='CL')
+    brokers = Organization.objects.filter(org_type='BR')
+
     context = {
         'clients': clients,
         'brokers': brokers
     }
     return render(request, 'transaction/index.html', context=context)
+
+def transaction(request):
+    if request.method == 'POST':
+        seller = Organization.objects.get(pk=request.POST['seller'])
+        buyer = Organization.objects.get(pk=request.POST['buyer'])
+        broker = Organization.objects.get(pk=request.POST['broker'])
+        date = request.POST['date']
+        item = request.POST['item']
+        quantity = request.POST['quantity']
+        weight = request.POST['weight']
+        rate = request.POST['rate']
+        brokerage_rate = request.POST['brokerage']
+
+        transaction = Transaction(seller=seller, buyer=buyer, broker=broker, date=date, item=item, quantity=quantity, weight=weight, rate=rate, brokerage_rate=brokerage_rate)
+        transaction.save()
+        return redirect('transaction:index')
+    
+    return redirect('transaction:index')
