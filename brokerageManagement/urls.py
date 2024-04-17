580c675d22287486cac3466a84a8469332a7535e
Tushhr <tusharmaheshwaritm6@gmail.com>
2024-04-17 21:38:05 +0530

    feat: create a page to show all the orgs broker has dealt with

--- brokerageManagement/urls.py
+++ brokerageManagement/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('transaction.urls')),
     path('admin/', admin.site.urls),
+    path('org/', include('organization.urls')),
 ]
--- organization/views.py
+++ organization/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from organization.models import Organization
+from transaction.models import Transaction
+
 # Create your views here.
+def index(request):
+    clients = Organization.objects.filter(org_type='CL').order_by('name')
+    context = {
+        'clients': clients,
+    }
+    return render(request, 'organization/index.html', context=context)
